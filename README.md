3a831111710278b2b218bed456be1f4de284b345
lisider <1368317399@qq.com>
2018-04-13 07:14:26 -0700

    change Framework

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+#tmp 临时文件存放处
+#bin 我的一些脚本工具之类
+#pro 我的工作工程之类
+#github 我的github工程之类
+
+
+
+```c
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/lisider/myhome/master/config.sh)"
+
+```
---
+++ config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+AddUser(){
+    USERNAME=$1
+    sudo useradd ${USERNAME} -s /bin/bash -G sudo ;sudo passwd ${USERNAME}
+    sudo git clone https://github.com/lisider/myhome.git /home/${USERNAME}
+    sudo chown ${USERNAME}:${USERNAME} /home/${USERNAME} -R
+    cp /etc/skel/*  /home/${USERNAME}/
+    #ln -s /home/${USERNAME}/.config/.profile /home/${USERNAME}/.profile
+    #ln -s /home/${USERNAME}/.config/.bashrc /home/${USERNAME}/.bashrc
+}
+
+SudoWithoutPasswd(){
+    echo -e "\033[32m Do $FUNCNAME ... Start\033[0m"
+    echo ${PASSWD} | sudo sed -i "\$a $USER ALL=(ALL:ALL) NOPASSWD:ALL" /etc/sudoers 
+    echo -e "\033[32m Do $FUNCNAME ... End\033[0m"
+}
+
+
+Main(){
+
+    AddUser $*
+    SudoWithoutPasswd
+
+}
+
+Main $*
